8df151d59114b20d38c454b2fbe0810f0daf3174
superwingcool <superwingcool@gmail.com>
2020-01-11 21:31:31 +0800

    refactor, using state moder

--- src/main/java/com.thoughtworks/fizz/buzz/FizzBuzzDemo.java
+++ src/main/java/com.thoughtworks/fizz/buzz/FizzBuzzDemo.java
@@ -15,37 +15,147 @@ public class FizzBuzzDemo {
 
     public String fizzBuzz(int number) {
 
-        String numberStr = String.valueOf(number);
-        String result = "";
+        FizzBuzzContext fizzBuzzContext = new FizzBuzzContext(FizzBuzzState.START_FIZZ_STATE,
+                number, "");
+        while (fizzBuzzContext.getState().process(fizzBuzzContext)) ;
+        return fizzBuzzContext.getResult();
 
-        if (number % 3 == 0) {
-            result += "Fizz";
-        }
-
-        if (number % 5 == 0 && !numberStr.contains("7")) {
-            result += "Buzz";
-        }
-
-        if (number % 7 == 0) {
-            result += "Whizz";
-        }
-
-        if (numberStr.contains("5") && !numberStr.contains("7")) {
-            if (result.contains("Fizz")) {
-                result = result.replaceAll("Fizz", "");
-            } else if (!result.contains("Buzz")) {
-                result += "Buzz";
-            }
-
-        } else if (numberStr.contains("3")) {
-            if (result.length() > 0) {
-                result = result.replaceAll(result, "");
-            }
-            result += "Fizz";
-        }
-
-        return result.isEmpty() ? numberStr : result;
     }
+
+    private enum FizzBuzzState implements State {
+
+
+
+        START_FIZZ_STATE {
+            public boolean process(FizzBuzzContext context) {
+                if (context.getNumber() % 3 == 0) {
+                    context.processResult(FIZZ);
+                }
+                context.setState(BUZZ_STATE);
+                return true;
+            }
+        },
+
+        BUZZ_STATE {
+            public boolean process(FizzBuzzContext context) {
+                int number = context.getNumber();
+                if (number % 5 == 0 && !String.valueOf(number).contains("7")) {
+                    context.processResult(BUZZ);
+                }
+                context.setState(WHIZZ_STATE);
+                return true;
+            }
+        },
+
+        WHIZZ_STATE {
+            public boolean process(FizzBuzzContext context) {
+                context.setState(CONTAIN_FIVE_NOT_SEVEN_STATE);
+                int number = context.getNumber();
+                if (number % 7 == 0) {
+                    context.processResult(WHIZZ);
+                }
+                return true;
+            }
+        },
+
+        CONTAIN_FIVE_NOT_SEVEN_STATE {
+            public boolean process(FizzBuzzContext context) {
+
+                String numberStr = String.valueOf(context.getNumber());
+                if (numberStr.contains("5") && !numberStr.contains("7")) {
+                    String result = context.getResult();
+                    if (result.contains(FIZZ)) {
+                        context.setResult(result.replaceAll(FIZZ, ""));
+                    } else if (!result.contains(BUZZ)) {
+                        context.processResult(BUZZ);
+                    }
+                    context.setState(END);
+                    return false;
+                }else {
+                    context.setState(CONTAIN_THREE_STATE);
+                    return true;
+                }
+            }
+        },
+
+        CONTAIN_THREE_STATE {
+            public boolean process(FizzBuzzContext context) {
+                String numberStr = String.valueOf(context.getNumber());
+                if (numberStr.contains("3")) {
+                    if (context.getResult().length() > 0) {
+                        context.setResult("");
+                    }
+                    context.processResult(FIZZ);
+                }
+                context.setState(END);
+                return false;
+            }
+        },
+
+        END {
+            public boolean process(FizzBuzzContext context) {
+                context.setState(null);
+                String result = context.getResult();
+                context.setResult(result.isEmpty() ? String.valueOf(context.number) : result);
+                return false;
+            }
+        };
+
+
+        @Override
+        public abstract boolean process(FizzBuzzContext context);
+    }
+
+    private interface State {
+
+        static final String WHIZZ = "Whizz";
+        static final String BUZZ = "Buzz";
+        static final String FIZZ = "Fizz";
+
+        boolean process(FizzBuzzContext context);
+    }
+
+
+    private class FizzBuzzContext {
+
+        private State state;
+
+        private int number;
+
+        private String result;
+
+        public FizzBuzzContext(State state, int number, String result) {
+            this.state = state;
+            this.number = number;
+            this.result = result;
+        }
+
+
+        public State getState() {
+            return state;
+        }
+
+        public void setState(State state) {
+            this.state = state;
+        }
+
+        public int getNumber() {
+            return number;
+        }
+
+        public String getResult() {
+            return this.result.isEmpty() ? String.valueOf(this.number) : this.result;
+        }
+
+        public void setResult(String result) {
+            this.result = result;
+        }
+
+        public void processResult(String newResult) {
+            this.result += newResult;
+        }
+    }
+
 
 
 
